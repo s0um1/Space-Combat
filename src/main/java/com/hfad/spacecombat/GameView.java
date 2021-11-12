593adee54d8b720e24788eb67b828c505273a325
s0um1 <48277456+s0um1@users.noreply.github.com>
2021-11-12 12:20:45 +0530

    Add files via upload

---
+++ src/main/java/com/hfad/spacecombat/GameView.java
@@ -0,0 +1,315 @@
+package com.hfad.spacecombat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameView extends SurfaceView implements Runnable {
+
+    private Thread thread;
+    private boolean isPlaying, isGameOver = false;
+    private int screenX, screenY, score = 0;
+    public static float screenRatioX, screenRatioY;
+    private Paint paint;
+    private Enemy[] enemies;
+    private SharedPreferences prefs;
+    private Random random;
+    private SoundPool soundPool;
+    private List<Bullet> bullets;
+    private int sound;
+    private Flight flight;
+    private GameActivity activity;
+    private ArrayList<Star> stars = new ArrayList<Star>();
+    private Canvas canvas;
+    private SurfaceHolder surfaceHolder;
+
+    public GameView(GameActivity activity, int screenX, int screenY) {
+        super(activity);
+
+        this.activity = activity;
+
+        prefs = activity.getSharedPreferences("game", Context.MODE_PRIVATE);
+
+        int starNums = 100;
+        for (int i = 0; i < starNums; i++) {
+            Star s  = new Star(screenX, screenY);
+            stars.add(s);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+
+        } else
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+
+        sound = soundPool.load(activity, R.raw.shoot, 1);
+
+        this.screenX = screenX;
+        this.screenY = screenY;
+        screenRatioX = 1920f / screenX;
+        screenRatioY = 1080f / screenY;
+
+        flight = new Flight(this, screenY, getResources());
+
+        bullets = new ArrayList<>();
+
+
+        paint = new Paint();
+        paint.setTextSize(128);
+        paint.setColor(Color.WHITE);
+
+        surfaceHolder = getHolder();
+
+        enemies = new Enemy[4];
+
+        for (int i = 0;i < 4;i++) {
+
+            Enemy enemy = new Enemy(getResources());
+            enemies[i] = enemy;
+
+        }
+
+        random = new Random();
+
+    }
+
+    @Override
+    public void run() {
+
+        while (isPlaying) {
+
+            update ();
+            draw ();
+            sleep ();
+
+        }
+
+    }
+
+    private void update () {
+
+        for (Star s : stars) {
+            s.update(flight.getSpeed());
+        }
+
+        if (flight.isGoingUp)
+            flight.y -= 30 * screenRatioY;
+        else
+            flight.y += 30 * screenRatioY;
+
+        if (flight.y < 0)
+            flight.y = 0;
+
+        if (flight.y >= screenY - flight.height)
+            flight.y = screenY - flight.height;
+
+        List<Bullet> trash = new ArrayList<>();
+
+        for (Bullet bullet : bullets) {
+
+            if (bullet.x > screenX)
+                trash.add(bullet);
+
+            bullet.x += 50 * screenRatioX;
+
+            for (Enemy enemy : enemies) {
+
+                if (Rect.intersects(enemy.getCollisionShape(),
+                        bullet.getCollisionShape())) {
+
+                    score++;
+                    enemy.x = -500;
+                    bullet.x = screenX + 500;
+                    enemy.wasShot = true;
+
+                }
+
+            }
+
+        }
+
+        for (Bullet bullet : trash)
+            bullets.remove(bullet);
+
+        for (Enemy enemy : enemies) {
+
+            enemy.x -= enemy.speed;
+
+            if (enemy.x + enemy.width < 0) {
+
+                if (!enemy.wasShot) {
+                    isGameOver = true;
+                    return;
+                }
+
+                int bound = (int) (30 * screenRatioX);
+                enemy.speed = random.nextInt(bound);
+
+                if (enemy.speed < 10 * screenRatioX)
+                    enemy.speed = (int) (10 * screenRatioX);
+
+                enemy.x = screenX;
+                enemy.y = random.nextInt(screenY - enemy.height);
+
+                enemy.wasShot = false;
+            }
+
+            if (Rect.intersects(enemy.getCollisionShape(), flight.getCollisionShape())) {
+
+                isGameOver = true;
+                return;
+            }
+
+        }
+
+    }
+
+    private void draw () {
+
+        if (surfaceHolder.getSurface().isValid()) {
+            canvas = surfaceHolder.lockCanvas();
+            canvas.drawColor(Color.BLACK);
+
+            //setting the paint color to white to draw the stars
+            paint.setColor(Color.WHITE);
+
+            //drawing all stars
+            for (Star s : stars) {
+                paint.setStrokeWidth(s.getStarWidth());
+                canvas.drawPoint(s.getX(), s.getY(), paint);
+            }
+
+            for (Enemy enemy : enemies)
+                canvas.drawBitmap(enemy.getEnemy(), enemy.x, enemy.y, paint);
+
+            canvas.drawText(score + "", screenX / 2f, 164, paint);
+
+            if (isGameOver) {
+                isPlaying = false;
+                canvas.drawBitmap(flight.getDead(), flight.x, flight.y, paint);
+                paint.setTextSize(200);
+                paint.setTextAlign(Paint.Align.CENTER);
+
+                int yPos=(int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2));
+                canvas.drawText("Game Over",canvas.getWidth()/2,yPos,paint);
+
+                getHolder().unlockCanvasAndPost(canvas);
+                saveIfHighScore();
+                waitBeforeExiting ();
+                return;
+            }
+
+            canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
+
+            for (Bullet bullet : bullets)
+                canvas.drawBitmap(bullet.bullet, bullet.x, bullet.y, paint);
+
+            getHolder().unlockCanvasAndPost(canvas);
+
+        }
+
+    }
+
+    private void waitBeforeExiting() {
+
+        try {
+            Thread.sleep(3000);
+            activity.startActivity(new Intent(activity, MainActivity.class));
+            activity.finish();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void saveIfHighScore() {
+
+        if (prefs.getInt("highscore", 0) < score) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("highscore", score);
+            editor.apply();
+        }
+
+    }
+
+    private void sleep () {
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void resume () {
+
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    public void pause () {
+
+        try {
+            isPlaying = false;
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (event.getX() < screenX / 2) {
+                    flight.isGoingUp = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                flight.isGoingUp = false;
+                if (event.getX() > screenX / 2)
+                    flight.toShoot++;
+                break;
+        }
+
+        return true;
+    }
+
+    public void newBullet() {
+
+        if (!prefs.getBoolean("isMute", false))
+            soundPool.play(sound, 1, 1, 0, 0, 1);
+
+        Bullet bullet = new Bullet(getResources());
+        bullet.x = flight.x + flight.width;
+        bullet.y = flight.y + (flight.height / 2);
+        bullets.add(bullet);
+
+    }
+}
